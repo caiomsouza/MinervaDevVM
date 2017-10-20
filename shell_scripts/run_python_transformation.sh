9355e9d1e7b66f9542335db9d1e89b65883ab849
Caio Moreno <caiomsouza@gmail.com>
2017-10-20 23:58:16 +0200

    Added Scripts

---
+++ shell_scripts/run_python_transformation.sh
@@ -0,0 +1,3 @@
+cd /home/minerva/Pentaho/design-tools/data-integration
+./pan.sh -file=/home/minerva/Desktop/Test/t_test_python_step.ktr -level=Basic
+
---
+++ shell_scripts/run_r_transformation.sh
@@ -0,0 +1,2 @@
+cd /home/minerva/Pentaho/design-tools/data-integration
+./pan.sh -file=/home/minerva/Desktop/Test/t_r_script_hello_world.ktr -level=Basic
